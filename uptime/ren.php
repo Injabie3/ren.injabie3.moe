47aedaa95d17487ddf4938eb747f578945fa8bcc
Lui <injabie3@gmail.com>
2017-05-21 13:33:24 -0700

    Use cURL instead of file_get_contents() - References issue #1.

--- uptime/ren.php
+++ uptime/ren.php
@@ -1,3 +1,8 @@
 <?php
-	echo file_get_contents("http://aws.injabie3.moe/uptime/ren.php");
+    $url = "http://aws.injabie3.moe/uptime/ren.php";
+    $ch = curl_init( $url );
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+    $result = curl_exec($ch);
+    curl_close($ch);
+    echo $result;
 ?>
