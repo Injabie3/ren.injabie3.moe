8cd25ccbc43b9167b26da281f8a03a2932b26719
Lui <injabie3@gmail.com>
2017-06-01 21:05:50 -0700

    [Footer] Adjust CSS, add commit info. - References #3.

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,12 @@
+<?php
+    $hash_short = exec('git rev-parse --short HEAD');
+    $hash_long = exec('git rev-parse --long HEAD');
+    
+?>
 <footer class="footer">
 	<div class="container">
         <p class="text-muted">
-            © <a href="https://injabie3.moe/">Injabie3</a> (2016-2017).  Ren bot maintained by @sedruk, @LeSpaz, and @Injabie3.
+            © <a href="https://injabie3.moe/">Injabie3</a> (2016-2017).  Ren bot maintained by @sedruk, @LeSpaz, and @Injabie3. Commit <a href="https://github.com/Injabie3/discord.injabie3.moe/commit/<?php echo $hash_long; ?>"><code><?php echo $hash_short; ?></code></a>
 			<?php
             if (strtolower($_SERVER['HTTP_HOST']) == "discord.injabie3.moe")
 			{
